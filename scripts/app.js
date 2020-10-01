4e076956b84929348a879327cb56bc1ff927957f
spofy30 <hichammiftah8004@gmail.com>
2020-10-01 23:32:28 +0100

    first commit

---
+++ scripts/app.js
@@ -0,0 +1,12 @@
+gsap.from('.col-1', {
+    opacity: 0,
+    duration: 1,
+    x: -50,
+    ease: 'Power2.easeInOut'
+});
+gsap.from('.col-2', {
+    opacity: 0,
+    duration: 1,
+    x: 150,
+    ease: 'Power2.easeInOut'
+});
